81416b5262535b5239f8f1b1ec391345af8e03c5
calacc <calacc314@gmail.com>
2023-04-02 23:40:01 +0300

    added the score printing functionality to the loopUnrolling class

--- BenchmarkingProject/src/testbench/TestCPURecursionLoopUnrolling.java
+++ BenchmarkingProject/src/testbench/TestCPURecursionLoopUnrolling.java
@@ -19,13 +19,14 @@ public class TestCPURecursionLoopUnrolling {
         bench.warmUp();
 
         timer.start();
-//        bench.run(false);
-        bench.run(true, 1);
+        bench.run(false);
+//        bench.run(true, 1);
 //        bench.run(true, 3);
 
         long time=timer.stop();
         log.write("Finished in ", time, TimeUnit.units.ms);
         log.write(((CPURecursionLoopUnrolling) bench).getParameters());
+        log.write("Score:", workload*10000.0/time);
 
 
     }
